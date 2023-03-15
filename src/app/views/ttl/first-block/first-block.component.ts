b637463ed32fa73f30fe5678b500874b21d8ad3c
ericklopez90 <ericklopeza90@gmail.com>
2023-03-15 00:48:12 -0600

    casi listo

--- src/app/views/ttl/first-block/first-block.component.ts
+++ src/app/views/ttl/first-block/first-block.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-first-block',
   templateUrl: './first-block.component.html',
-  styleUrls: ['./first-block.component.scss']
+  styleUrls: ['./first-block.component.scss'],
 })
 export class FirstBlockComponent {
 
 }
+
+
